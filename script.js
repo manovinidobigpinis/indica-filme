9b5c2e03329e35f5959948fa2bdc0831411ebe00
Vinicius Tomazoni Renaud <viniestudamuitomesmo@gmail.com>
2026-05-26 11:00:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,140 @@
+const produtos = [
+    {
+        nome: 'Suberbad',
+        ano: '2007',
+        genero: 'Comédia',
+        imagem: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZbbx-Azs5kZq1vV1tOSzxJPkTNjAUk1xnZg&s'
+    },
+    {
+        nome: 'Gente Grande',
+        ano: '2010',
+        genero: 'Comédia',
+        imagem: ''
+    },
+    {
+        nome: 'Eurotrip',
+        ano: '2004',
+        genero: 'Comédia',
+        imagem: ''
+    },
+    {
+        nome: 'American pie',
+        ano: '1999',
+        genero: 'Comédia',
+        imagem: ''
+    },
+    {
+        nome: 'Projeto X',
+        ano: '2012',
+        genero: 'Comédia',
+        imagem: ''
+    },
+    {
+        nome: '10 Coisas que Eu Odeio em Você',
+        ano: '1999',
+        genero: 'Romance',
+        imagem: ''
+    },
+    {
+        nome: '(500) Dias com ela',
+        ano: '2009',
+        genero: 'Romance',
+        imagem: ''
+    },
+    {
+        nome: 'Lalaland',
+        ano: '2016',
+        genero: 'Romance',
+        imagem: ''
+    },
+    {
+        nome: 'Todos menos você',
+        ano: '2023',
+        genero: 'Romance',
+        imagem: ''
+    },
+    {
+        nome: 'Como se fosse a primeira vez',
+        ano: '2004',
+        genero: 'Romance',
+        imagem: ''
+    },
+    {
+        nome: 'Gigantes de aço',
+        ano: '2011',
+        genero: 'Ação',
+        imagem: ''
+    },
+    {
+        nome: 'Django livre',
+        ano: '2012',
+        genero: 'Ação',
+        imagem: ''
+    },
+    {
+        nome: 'Tropa de elite',
+        ano: '2007',
+        genero: 'Ação',
+        imagem: ''
+    },
+    {
+        nome: 'Os bad boys',
+        ano: '1995',
+        genero: 'Ação',
+        imagem: ''
+    },
+    {
+        nome: 'Top Gun: Maverick',
+        ano: '2022',
+        genero: 'Ação',
+        imagem: ''
+    },
+    {
+        nome: 'Pulp Fiction',
+        ano: '1994',
+        genero: 'Ficção',
+        imagem: ''
+    },
+    {
+        nome: 'Duna',
+        ano: '2021',
+        genero: 'Ficção',
+        imagem: ''
+    },
+    {
+        nome: 'Interestelar',
+        ano: '2014',
+        genero: 'Ficção',
+        imagem: ''
+    },
+    {
+        nome: 'Tudo em todo lugar ao mesmo tempo',
+        ano: '2022',
+        genero: 'Ficção',
+        imagem: ''
+    },
+    {
+        nome: 'Donie Darko',
+        ano: '1994',
+        genero: 'Ficção',
+        imagem: ''
+    },
+]
+
+function buscar(){
+    const genero = document.getElementById('genero')
+    const ano = document.getElementById('ano')
+
+    if (genero >= 1990){
+        
+    }
+    else if (genero >= 2000){
+
+    }
+    else if (genero >= 2010){
+
+    }
+    else if (genero >= 2020){
+
+    }
+}
